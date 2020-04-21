2aef88b0f72d2d5fbfb51e3a5ab1a8b0f044bc58
Brian Kang <1kangyun1@gmail/com>
2020-04-21 18:52:41 -0400

    fixing

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function NavigationBar() {
@@ -8,8 +8,18 @@ export default function NavigationBar() {
       <Navbar.Brand href="/">Brian Kang</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Link style={linkStyle} to="/projects">link pro</Link>
-        <Link style={linkStyle} to="/resume">link resu</Link>
+        <Nav className="mr-auto">
+          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Link to="/projects">link pro</Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
+          <Link to="/resume">link resu</Link>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
