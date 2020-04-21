11a44e1b7995625c85aa2b4aa01fa0640eea111a
Brian Kang <1kangyun1@gmail/com>
2020-04-21 18:54:39 -0400

    fixing

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 export default function NavigationBar() {
   return (
@@ -9,10 +8,8 @@ export default function NavigationBar() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/projects">Projects</Nav.Link>
-          <Link to="/projects">link pro</Link>
-          <Nav.Link href="/resume">Resume</Nav.Link>
-          <Link to="/resume">link resu</Link>
+          <Nav.Link to="/projects">Projects</Nav.Link>
+          <Nav.Link to="/resume">Resume</Nav.Link>
         </Nav>
         <Nav>
           <Nav.Link href="#deets">More deets</Nav.Link>
